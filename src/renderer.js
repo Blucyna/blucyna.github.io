562e85a7d14ebe90b95f7468527cb218c2e4804a
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2020-04-30 14:25:50 +0200

    rendering

--- src/renderer.js
+++ src/renderer.js
@@ -4,11 +4,23 @@ class Renderer {
   constructor(viewsDir) {
     this.viewsDir = viewsDir;
 
-    nunjucks.configure(this.viewsDir, { autoescape: true });
+    this.renderer = nunjucks.configure(this.viewsDir, { autoescape: true });
   }
 
   render(view, data) {
-    return nunjucks.render(view, data);
+    return this.renderer.render(view, data);
+  }
+
+  renderAsync(view, data) {
+    return new Promise((resolve, reject) => {
+      this.renderer.render(view, data, function(err, res) {
+        if(err) {
+          return reject(err);
+        }
+
+        return resolve(res);
+      });
+    })
   }
 }
 
