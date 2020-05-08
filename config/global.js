64800232e6e4b4e0c0ec236b3f62c78f41f601e6
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2020-05-08 13:07:36 +0200

    lazyload

--- config/global.js
+++ config/global.js
@@ -3,7 +3,7 @@ module.exports = {
   avatar: 'img/avatar.jpg',
   avatar_webp: 'img/avatar.webp',
   name: 'Lucyna Bugara',
-  subtitle: 'UX / UI designer',
+  subtitle: 'UX / UI & Web designer',
   description: 'I started my career in IT as Project Manager. In time I realised that the most exciting part of the project for me is creative planning of UX/UI and Web Design. I\'ve always had multiple visions and ideas instantly while looking at the brief and tried to focus on what is going on in users head, how I can help him. I decided that my imagination is way to big to not start challenge it into the design :) ',
   socials: [
     // {icon: 'fa-facebook', name: 'facebook', address: 'https://facebook.com'},
@@ -12,9 +12,9 @@ module.exports = {
     {icon: 'fa-linkedin', name: 'linkedin', address: 'https://www.linkedin.com/in/lbugara/'},
   ],
   meta: {
-    title: 'blucy.pl - Lucyna Bugara UX / UI designer',
+    title: 'blucy.pl - Lucyna Bugara UX / UI & Web designer',
     keywords: 'portfolio, ux, ui, webdesign, design, graphic',
-    description: 'Lucyna Bugara - UX / UI designer, need a design? contact me'
+    description: 'Lucyna Bugara - UX / UI & Web designer, need a design? contact me'
   },
   contact: {
     email: 'lucyna.bugara@gmail.com',
